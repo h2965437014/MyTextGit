b46e7f161a4a7dcb45fac1245e003dd9cb8e2ed1
hu <295437014@qq.com>
2016-05-12 16:17:51 +0800

    Initial Commit

---
+++ MyGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyGit
+//
+//  Created by hu on 16/5/12.
+//  Copyright © 2016年 Howie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyGit/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MyGit
+//
+//  Created by hu on 16/5/12.
+//  Copyright © 2016年 Howie. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
